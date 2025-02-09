695af091db3ebd65406197b669faa3366f9293dc
Hendra Arfiansyah <hendra.arfiansyah@bukalapak.com>
2025-02-09 19:06:22 +0700

    separate page component

---
+++ components/invoices/EditInvoice.tsx
@@ -0,0 +1,130 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import dayjs from 'dayjs';
+import { redirect, useParams } from 'next/navigation';
+import { Box, Divider, Paper, Stack, Typography } from '@mui/material';
+import InvoiceForm from '~/components/invoices/InvoiceForm';
+import { InvoiceFormData, InvoiceStatus } from '~/lib/types/invoice';
+import { useInvoices } from '~/hooks/useInvoices';
+
+export default function EditInvoice() {
+  const params = useParams<{ id: string }>();
+  const { invoices, getInvoiceById, fetchInvoices, updateInvoice } =
+    useInvoices();
+
+  const fetchedRef = useRef(false);
+  const [isInitialized, setIsInitialized] = useState(false);
+  const [invoice, setInvoice] = useState<InvoiceFormData>({
+    name: '',
+    number: '',
+    dueDate: null,
+    amount: 0,
+    status: InvoiceStatus.PAID,
+  });
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      fetchInvoices().then(() => (fetchedRef.current = true));
+    }, 0);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+  useEffect(() => {
+    if (!fetchedRef.current) {
+      return;
+    }
+
+    const invoice = getInvoiceById(params.id);
+
+    if (!invoice) {
+      redirect('/');
+    }
+
+    setInvoice({
+      name: invoice.name,
+      number: invoice.number,
+      dueDate: dayjs(invoice.dueDate),
+      amount: invoice.amount,
+      status: invoice.status,
+    });
+  }, [invoices]);
+
+  useEffect(() => {
+    if (!fetchedRef.current) {
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      setIsInitialized(true);
+    }, 0);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [invoice]);
+
+  const handleUpdate = (invoice: Partial<InvoiceFormData>) => {
+    if (!isInitialized) {
+      return;
+    }
+
+    setInvoice((prevInvoice) => ({
+      ...prevInvoice,
+      ...invoice,
+    }));
+  };
+
+  const clearForm = () => {
+    setInvoice({
+      name: '',
+      number: '',
+      dueDate: null,
+      amount: 0,
+      status: InvoiceStatus.PAID,
+    });
+  };
+
+  const handleSave = (invoice: InvoiceFormData) => {
+    updateInvoice(params.id, {
+      name: invoice.name,
+      number: invoice.number,
+      amount: invoice.amount,
+      status: invoice.status!,
+      dueDate: invoice.dueDate!.format('YYYY-MM-DD'),
+    });
+
+    clearForm();
+  };
+
+  return (
+    <Stack gap={4}>
+      <Typography variant="h4" component="h1">
+        Edit Invoice
+      </Typography>
+      {isInitialized && (
+        <Paper>
+          <Box
+            sx={{
+              py: '15px',
+              px: '26px',
+            }}
+          >
+            <Typography variant="h5" component="h2">
+              Invoice Form
+            </Typography>
+          </Box>
+          <Divider />
+          <Box sx={{ p: '26px' }}>
+            <InvoiceForm
+              invoice={invoice}
+              onUpdate={handleUpdate}
+              onSave={handleSave}
+            />
+          </Box>
+        </Paper>
+      )}
+    </Stack>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,5 @@
-'use client';
+import MyInvoice from '~/components/invoices/MyInvoice';
 
-import { useEffect } from 'react';
-import { Stack, Typography } from '@mui/material';
-import InvoiceList from '~/components/invoices/InvoiceList';
-import { useInvoices } from '~/hooks/useInvoices';
-import InvoiceListFilter from '~/components/invoices/InvoiceListFilter';
-
-export default function MyInvoice({
-  searchParams,
-}: {
-  searchParams: Promise<Record<string, string | undefined>>;
-}) {
-  const { invoices, fetchInvoices, deleteInvoice } = useInvoices();
-
-  useEffect(() => {
-    async function initData() {
-      const query = await searchParams;
-      fetchInvoices({
-        keywords: query?.keywords || '',
-        status: query?.status || '',
-      });
-    }
-
-    initData();
-  }, [searchParams]);
-
-  return (
-    <Stack gap={4}>
-      <Stack direction="row" justifyContent="space-between">
-        <Typography variant="h4" component="h1">
-          My Invoices
-        </Typography>
-        <InvoiceListFilter />
-      </Stack>
-
-      <InvoiceList invoices={invoices} onDelete={deleteInvoice} />
-    </Stack>
-  );
+export default function MyInvoicePage() {
+  return <MyInvoice />;
 }
